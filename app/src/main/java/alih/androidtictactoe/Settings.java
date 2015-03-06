ab1d65ee01e1abc8dc8f4849965c9f19c7e8e2bd
Ali Homafar <home.isfar@gmail.com>
2015-03-06 14:34:03 -0600

    Preferences now show in sub-text what the currently selected option is.

--- app/src/main/java/alih/androidtictactoe/Settings.java
+++ app/src/main/java/alih/androidtictactoe/Settings.java
@@ -1,6 +1,10 @@
 package alih.androidtictactoe;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 /**
@@ -13,5 +17,43 @@ public class Settings extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         getPreferenceManager().setSharedPreferencesName("ttt_prefs");
         addPreferencesFromResource(R.xml.preferences);
+
+        // Code to help show what the preference is from the drop-down menu itself
+        final SharedPreferences prefs =
+                getSharedPreferences("ttt_prefs", MODE_PRIVATE);
+
+        final ListPreference difficultyLevelPref = (ListPreference) findPreference("difficulty_level");
+        String difficulty = prefs.getString("difficulty_level",
+                getResources().getString(R.string.difficulty_expert));
+        difficultyLevelPref.setSummary((CharSequence) difficulty);
+        difficultyLevelPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                difficultyLevelPref.setSummary((CharSequence) newValue);
+                // Since we are handling the pref, we must save it
+                SharedPreferences.Editor ed = prefs.edit();
+                ed.putString("difficulty_level", newValue.toString());
+                ed.apply();
+                return true;
+            }
+        });
+
+        final EditTextPreference victoryMessagePref = (EditTextPreference) findPreference("victory_message");
+        String victoryM = prefs.getString("victory_message",
+                getResources().getString(R.string.result_human_wins));
+        victoryMessagePref.setSummary((CharSequence) victoryM);
+        victoryMessagePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                victoryMessagePref.setSummary((CharSequence) newValue);
+                //since we are handling the pref here, save it
+                SharedPreferences.Editor ed = prefs.edit();
+                ed.putString("result_human_wins", newValue.toString());
+                ed.apply();
+                return true;
+            }
+        });
     }
 }
